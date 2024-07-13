e95c3fc6fbc2c4bc1325ee757d99711b60304f19
everton <42036499+notrevesg07@users.noreply.github.com>
2024-07-12 21:33:35 -0300

    v03
    opções do menu

--- CartorioEbac.cpp
+++ CartorioEbac.cpp
@@ -8,31 +8,40 @@ int main()
 	setlocale(LC_ALL, "portuguese"); 
 	// Definiu variável 
 	int opcao=0; 
-	//menu
-	printf("### Cartório Everton ### \n");
-	printf("Escolha a opção desejada do menu: \n\n");
-	printf("\t1 - registar nome\n");
-	printf("\t2 - consultar nome \n");
-	printf("\t3 - deletar nome  \n\n");
-	printf("Opção: ");
-	//Informação do usuário 
-	scanf("%d", &opcao);
-	system("cls");// limpar console
-	// Verificações das opções do menu 
-	if(opcao==1){
-		printf("Você escolheu inserir registros\n");
+	int loco=1;
+	//laço de repetiçõo
+	for(loco=1;loco=1;){
+		system("cls");// limpar console
+		//menu
+		printf("### Cartório Everton ### \n");
+		printf("Escolha a opção desejada do menu: \n\n");
+		printf("\t1 - registar nome\n");
+		printf("\t2 - consultar nome \n");
+		printf("\t3 - deletar nome  \n\n");
+		printf("Opção: ");
+		
+		//Informação do usuário 
+		scanf("%d", &opcao);
+		system("cls");// limpar console
+		
+		// Verificações das opções do menu 
+		if(opcao==1){
+			printf("Você escolheu inserir registros\n");
+			system("pause");
+		}
+		if(opcao==2){
+			printf("Você escolheu consultar os registros\n");
+			system("pause");
+		}
+		if(opcao==3){
+			printf("Você escolheu excluir registros\n");
+			system("pause");
+		}
+		if(opcao>=4 || opcao<=0 ){
+			printf("Você escolheu opção Que não existe \n");
+			system("pause");
+		}
+		printf(" Software livre SQN");
 	}
-	if(opcao==2){
-		printf("Você escolheu consultar os registros\n");
-	}
-	if(opcao==3){
-		printf("Você escolheu excluir registros\n");
-	}
-	if(opcao>=4 || opcao<=0 ){
-		printf("Você escolheu opção Que não existe \n");
-	}
-	
-	
-	printf(" Software livre SQN");
 	
 }
